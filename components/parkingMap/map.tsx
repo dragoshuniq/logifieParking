d7338bd29b11d0321db4532b2eeba7931683a389
Dragos Todoroscean <dtuniq@gmail.com>
2025-11-29 15:19:44 +0100

    ref

--- components/parkingMap/map.tsx
+++ components/parkingMap/map.tsx
@@ -1,12 +1,22 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
+import MapView from "react-native-maps";
+import { ThemedView } from "../ui/themed-view";
 
 export const ParkingMap = () => {
   return (
-    <View>
-      <Text>map</Text>
-    </View>
+    <ThemedView style={styles.container}>
+      <MapView style={styles.map} />
+    </ThemedView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
