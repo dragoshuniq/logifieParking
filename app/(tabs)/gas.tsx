7ee23241577edc35845a22958d66917f5682094c
Dragos Todoroscean <dtuniq@gmail.com>
2025-11-29 17:32:52 +0100

    ref

--- app/(tabs)/gas.tsx
+++ app/(tabs)/gas.tsx
@@ -1,7 +1,24 @@
-import { StyleSheet } from "react-native";
+import { useQueryClient } from "@tanstack/react-query";
+import { Pressable, StyleSheet, Text } from "react-native";
 
 export default function TabTwoScreen() {
-  return <></>;
+  const queryClient = useQueryClient();
+
+  return (
+    <Pressable>
+      <Pressable
+        style={{
+          marginTop: 100,
+          backgroundColor: "red",
+          padding: 10,
+          borderRadius: 10,
+        }}
+        onPress={() => queryClient.clear()}
+      >
+        <Text>Clear cache</Text>
+      </Pressable>
+    </Pressable>
+  );
 }
 
 const styles = StyleSheet.create({
--- components/parkingMap/map.tsx
+++ components/parkingMap/map.tsx
@@ -1,29 +1,15 @@
 import { getAllParkings } from "@/api/parking";
 import { useQuery } from "@tanstack/react-query";
-import React, { useEffect } from "react";
+import React from "react";
 import { StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import { ThemedView } from "../ui/themed-view";
 
 export const ParkingMap = () => {
-  const { data, refetch, isFetching } = useQuery({
+  const { data } = useQuery({
     queryKey: ["parkings"],
     queryFn: () => getAllParkings(),
-    refetchOnMount: false,
-    refetchOnWindowFocus: false,
-    refetchOnReconnect: false,
   });
-
-  useEffect(() => {
-    if (
-      !isFetching &&
-      (!data || !data.parkings || data.parkings.length === 0)
-    ) {
-      refetch();
-    }
-  }, [data, isFetching, refetch]);
-
-  console.log(data);
 
   return (
     <ThemedView style={styles.container}>
