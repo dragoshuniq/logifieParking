50a74a57bfa8e5b2e4b5a5f34e8d943456625760
Dragos Todoroscean <dtuniq@gmail.com>
2025-11-29 17:40:18 +0100

    ref

--- components/parkingMap/map.tsx
+++ components/parkingMap/map.tsx
@@ -1,19 +1,43 @@
 import { getAllParkings } from "@/api/parking";
+import { DEFAULT_COORDINATES } from "@/constants/app.const";
+import { Colors } from "@/constants/theme";
+import { useColorScheme } from "@/hooks/use-color-scheme";
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { StyleSheet } from "react-native";
-import MapView, { Marker } from "react-native-maps";
+import MapView from "react-native-map-clustering";
+import { Marker } from "react-native-maps";
 import { ThemedView } from "../ui/themed-view";
 
 export const ParkingMap = () => {
+  const theme = useColorScheme() ?? "light";
   const { data } = useQuery({
     queryKey: ["parkings"],
     queryFn: () => getAllParkings(),
   });
 
+  const clusterColor = Colors[theme].primary.DEFAULT;
+  const clusterTextColor = Colors[theme].primary.foreground;
+
   return (
     <ThemedView style={styles.container}>
-      <MapView style={styles.map}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: DEFAULT_COORDINATES.latitude,
+          longitude: DEFAULT_COORDINATES.longitude,
+          latitudeDelta: 8.5,
+          longitudeDelta: 8.5,
+        }}
+        clusterColor={clusterColor}
+        clusterTextColor={clusterTextColor}
+        radius={50}
+        minZoom={10}
+        maxZoom={20}
+        extent={512}
+        nodeSize={64}
+        animationEnabled
+      >
         {data?.parkings?.map((parking) => (
           <Marker
             key={parking._id}
---
+++ constants/app.const.ts
@@ -0,0 +1,6 @@
+export const TIMEZONE = "Europe/Warsaw";
+
+export const DEFAULT_COORDINATES = {
+  latitude: 50.2743132,
+  longitude: 19.0040024,
+};
