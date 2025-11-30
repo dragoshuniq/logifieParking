6eacd7ea1d4646964f9b9da79f3cbfa5112a7b3d
Dragos Todoroscean <dtuniq@gmail.com>
2025-11-30 15:28:30 +0100

    ref

--- constants/app.const.ts
+++ constants/app.const.ts
@@ -1,3 +1,6 @@
+import { Languages } from "@/providers/i18n";
+const HOME_URL = process.env.EXPO_PUBLIC_HOME_URL ?? "";
+
 export const TIMEZONE = "Europe/Warsaw";
 
 export const DEFAULT_COORDINATES = {
@@ -5,8 +8,29 @@ export const DEFAULT_COORDINATES = {
   longitude: 19.0040024,
 };
 
-export const ExternalLinks = {
-  home: process.env.EXPO_PUBLIC_HOME_URL ?? "",
-  androidApp: process.env.EXPO_PUBLIC_ANDROID_APP_URL ?? "",
-  iosApp: process.env.EXPO_PUBLIC_IOS_APP_URL ?? "",
+export const ExternalLinks = (language: Languages) => {
+  return {
+    Home: HOME_URL,
+    Contact: `${HOME_URL}/${language}${AppRoutes.Contact}`,
+    GetQuote: `${HOME_URL}/${language}${AppRoutes.GetQuote}`,
+  };
+};
+
+// TODO: verify latest routes
+export const AppRoutes = {
+  Home: "/",
+  AboutUs: "/about",
+  CustomerZone: "/customer",
+  CarrierZone: "/carrier",
+  Blog: "/blog",
+  Parking: "/parking",
+  Faq: "/faq",
+  Contact: "/contact",
+  PrivacyPolicy: "/privacy-policy",
+  TermsAndConditions: "/terms-and-conditions",
+  GetQuote: "/get-quote",
+  Fuel: "/fuel",
+  Careers: "/careers",
+  Sitemap: "/sitemap",
+  Robots: "/robots",
 };
